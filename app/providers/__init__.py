bf6e1f0ccf042228cb5c33c5b5b4c2397a23403a
b8vipvip <917931227@qq.com>
2026-07-27 21:02:54 +0800

    feat: register Simli provider

--- app/providers/__init__.py
+++ app/providers/__init__.py
@@ -1,6 +1,7 @@
 from app.providers.akool import AkoolProvider
 from app.providers.liveavatar import LiveAvatarProvider
 from app.providers.mock import MockProvider
+from app.providers.simli import SimliProvider
 from app.providers.tavus import TavusProvider
 
 PROVIDER_CLASSES = {
@@ -8,4 +9,5 @@ PROVIDER_CLASSES = {
     "tavus": TavusProvider,
     "akool": AkoolProvider,
     "liveavatar": LiveAvatarProvider,
+    "simli": SimliProvider,
 }
