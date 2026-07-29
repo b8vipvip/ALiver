737ae5aeaa5d7ded9a5bbae838228b397d0eca9e
b8vipvip <917931227@qq.com>
2026-07-29 21:53:15 +0800

    fix: install routed action duration alignment

--- bridge/__init__.py
+++ bridge/__init__.py
@@ -1,6 +1,7 @@
 """ALiver Windows Bridge package initialization."""
 
 from bridge.avatar_action_control import install_avatar_action_control_patch
+from bridge.avatar_action_duration_fix import install_avatar_action_duration_fix
 from bridge.vtube_motion import install_vtube_motion_patch
 from bridge.vtube_motion_control import install_vtube_motion_control_patch
 from bridge.vtube_motion_safety import install_vtube_motion_safety_patch
@@ -12,4 +13,5 @@ install_vtube_motion_patch()
 install_vtube_motion_safety_patch()
 install_vtube_motion_v2_patch()
 install_avatar_action_control_patch()
+install_avatar_action_duration_fix()
 install_vtube_motion_control_patch()
