5767486dbc740946377edd1d854a9eae55ead3be
b8vipvip <917931227@qq.com>
2026-07-29 20:42:03 +0800

    feat: install VTube natural motion patch

--- bridge/__init__.py
+++ bridge/__init__.py
@@ -1,5 +1,7 @@
 """ALiver Windows Bridge package initialization."""
 
+from bridge.vtube_motion import install_vtube_motion_patch
 from bridge.vtube_studio_auth_fix import install_vtube_studio_auth_fix
 
 install_vtube_studio_auth_fix()
+install_vtube_motion_patch()
