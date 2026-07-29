ae694767f98e1221c3ad45e7485460ac54a87ab0
b8vipvip <917931227@qq.com>
2026-07-29 20:42:49 +0800

    feat: install VTube motion command routing

--- bridge/__init__.py
+++ bridge/__init__.py
@@ -1,7 +1,9 @@
 """ALiver Windows Bridge package initialization."""
 
 from bridge.vtube_motion import install_vtube_motion_patch
+from bridge.vtube_motion_control import install_vtube_motion_control_patch
 from bridge.vtube_studio_auth_fix import install_vtube_studio_auth_fix
 
 install_vtube_studio_auth_fix()
 install_vtube_motion_patch()
+install_vtube_motion_control_patch()
